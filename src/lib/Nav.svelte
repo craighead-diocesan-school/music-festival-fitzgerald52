967bef3ab760b7f76495cdfa1f59fc708da25ba0
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-05-18 21:28:28 +1200

    Navigation
    i styled my navigation component to match the fun and extroverted design to catch the eye of the target audience (teens and young adults) by using bright contrasting colors and bold fonts that are easy to read to help low vision users

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,32 @@
 <nav>
   <a href="/">Home</a>
   <a href="/acts">Acts</a>
-  <a href="/accommodation">accommodation</a>
-  <a href="/tickets">Tickets </a>
+  <a href="/accommodation">Accommodation</a>
+  <a class="ticket" href="/tickets">Tickets </a>
 </nav>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Krona+One&family=Rock+Salt&display=swap');
+  nav {
+    background-color: rgb(16, 218, 243);
+    padding: 25px;
+    display: flex;
+    justify-content: space-around;
+  }
+  a {
+    padding-left: 50px;
+    padding-right: 50px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    color: black;
+    font-family: 'Krona One', sans-serif;
+    font-size: large;
+    font-weight: 800;
+  }
+  .ticket {
+    border-radius: 15%;
+
+    background-color: rgb(246, 123, 69);
+    border-width: 20px;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import Header from '$lib/Header.svelte'
+  import Nav from '../lib/Nav.svelte'
 </script>
 
+<Nav />
 <Header />
 
 <main>
