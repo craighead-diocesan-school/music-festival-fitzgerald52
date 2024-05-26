b711dc01c055b9d9dedfaae74af239d615915f51
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2024-05-27 10:47:32 +1200

    header initial
    i spell checked the header and added the photo in the background of the header. after i tested it i later resized the photo. i added the font to my text and increased the space between the words to increase readability

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,6 +1,28 @@
-<header class="hero">
-  <div class="hero-body">
-    <h1 class="title">Welcome</h1>
-    <h2 class="subtitle">Welcome to my site</h2>
+<header>
+  <div class="container">
+    <h1>SUNBURST FESTIVAL</h1>
   </div>
 </header>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Rock+Salt&display=swap');
+  header {
+    background-image: url(aditya-chinchure.jpg);
+    background-size: contain;
+  }
+  .container {
+    width: 3000px;
+    height: 653px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  h1 {
+    color: aliceblue;
+    font-size: 6em;
+    text-align: center;
+    font-family: 'Rock Salt', cursive;
+    font-weight: 400;
+    word-spacing: 40px;
+  }
+</style>
